390fb5dfb380fae349cf042ff1a3fc3e26eb850e
Margherita De Toma <margheritadt.97@gmail.com>
2022-02-18 10:29:24 +0100

    modifica plot_sigma_vs_corr_len.py

--- Harmonic_oscillator/bootstrap/plot_sigma_vs_corr_len.py
+++ Harmonic_oscillator/bootstrap/plot_sigma_vs_corr_len.py
@@ -3,10 +3,9 @@ import matplotlib.pyplot as plt
 
 file_name = "./bootstrap_mean_sigma_len_file.txt"
 data = open(file_name,"r")
-x, dx, len_corr = np.loadtxt(data, unpack = True)
+sigma, len_corr = np.loadtxt(data, unpack = True)
 data.close()
 
-print(len_corr)
 
-plt.plot(len_corr, dx, '.')
+plt.plot(len_corr, sigma, '.')
 plt.show()
